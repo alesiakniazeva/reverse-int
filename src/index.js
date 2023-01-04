046d9a67dd35b085a333388b06445493d6285971
Alesia Kniazeva <alice.xx.green@gmail.com>
2023-01-04 13:57:41 +0300

    feat: added function reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  var i;
+  var n_reverse = '';
+  n = String(n);
+  for (i=n.length -1; i>=0; i--) {
+    if (n[i]!='-') {
+    n_reverse = n_reverse + n[i];
+    }
+  }
+  return Number(n_reverse);
 }
+
